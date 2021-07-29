8c8a80794adb896ccce379ef324ad87f22335e27
Fernando Dodino <fernando.dodino@gmail.com>
2021-07-28 23:00:40 -0300

    Fixing coverage

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { waitForAsync, TestBed } from '@angular/core/testing'
 import { RouterTestingModule } from '@angular/router/testing'
 
 import { AppComponent } from './app.component'
+import './app.module'
 
 describe('AppComponent', () => {
   beforeEach(waitForAsync(() => {
